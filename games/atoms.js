0c675d0a7e0f4c2b059531a9ecd07fd549898ec8
$your_name <$your_email>
2013-01-08 18:04:05 +0100

    added outline of program, OnClick function is to be changed

--- games/atoms.js
+++ games/atoms.js
@@ -1,34 +1,79 @@
-function OnClick()
+var player = 0;
+
+function get_the_actual_number_of_circles(i, j)
 {
+	var max_number = 0;
+	var actual_number = plansza[i][j];
+	if( plansza[i][j-1] > 0 || plansza[i][j-1] < 6)//jeden w lewo
+	{
+		max_number += 1;
+		acutal_number += 1;
+	}
+	if( plansza[i][j+1] > 0 || plansza[i][j+1] < 6 )// jeden w prawo
+	{
+		max_number += 1;
+		acutal_number += 1;
+	}
+	if( plansza[i-1][j] > 0 || plansza[i-1][j] < 6 )//jeden w dol
+	{
+		max_number += 1;
+		acutal_number += 1;
+	}
+	if( plansza[i+1][j] > 0 || plansza[i+1][j] < 6 )//jeden w gore
+	{
+		max_number += 1;
+		acutal_number += 1;
+	}
 	
 }
 
-function gameloop()
+
+function determine_the_changes( i, j )
 {
-	
+	if( player )
+	{
+		//czerwony
+		image_color="red";
+		//get the max number of circles, and actual number of circles:
+		get_the_actual_number_of_circles(i, j);
+	}
+	else
+	{
+		//zielony
+		image_color="greem";
+	}
 }
+
+function OnClick( i, j )
+{
+	if( plansza[i][j] == 0 || plansza[i][j] == player) // Jezeli mozliwe jest zrobienie ruchu.(mozliwe ze trzeba bedzie dodac, jezeli nie wybuchaja).
+	{
+		determine_the_changes( i, j );
+			
+		$("input[name=image"+i+"_"+j+"]")
+	}
+}
+
+
 
 function atoms_Init()
 {
-	//wpisanie tabelki z obrazkami w html'a
-//	$('.atoms_board').append()
-	
 	plansza = new Array();
 	dl_boku = 8;
-	s += "<center><table>";
+	table_string = "<center><table border=1>";
 	for( i = 0 ; i < dl_boku ; i++ )
 	{
 		plansza[i] = new Array();
-		s += "<tr>";
+		table_string += "<tr>";
 		for( j = 0 ; j < dl_boku ; j++ )
 		{
 			if( i == 0 || j == 0 )
 				plansza[i][j] = -1;
 			plansza[i][j] = 0;
-			s += "<td><img name=image"+i+"_"+j+" src=games/graphics/atoms/blank.jpg></td>";
+			table_string += "<td><img name=image"+i+"_"+j+" src=games/graphics/atoms/blank.jpg onclick='OnClick("+i+","+j+")'></td>";
 		}
-		s+="</tr>";
+		table_string+="</tr>";
 	}
-	s+= "</table></center>";
-	gameloop();
-}
+	table_string+= "</table></center>";
+	$("#atoms_board").append(table_string);
+}	
